35a8315dc5200d4f3697e1b89806b07052662892
shekosakr <mmdsakr942@gmail.com>
2025-03-03 02:21:45 +0200

    core package committ

--- lib/velora_app.dart
+++ lib/velora_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class VeloraStore extends StatelessWidget {
   const VeloraStore({super.key});
@@ -6,12 +7,25 @@ class VeloraStore extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Velora Store',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Scaffold(appBar: AppBar(title: Text("Velora Store"))));
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          title: 'Velora Store',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text("Velora Store"),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
