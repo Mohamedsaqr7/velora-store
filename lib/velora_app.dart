25150dd48ec7efbe264da981469557bafe9d24d1
shekosakr <mmdsakr942@gmail.com>
2025-03-04 01:04:34 +0200

    splash screen done

--- lib/velora_app.dart
+++ lib/velora_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:velora/core/app/connectivity_plus.dart';
 import 'package:velora/core/app/env_variables.dart';
 import 'package:velora/core/common/no_network_screen.dart';
@@ -9,18 +10,24 @@ class VeloraStore extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Velora Store',
-      debugShowCheckedModeBanner: EnvVariable.instance.debugMood,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('dsadsa'),
-        ),
-      ),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MaterialApp(
+            title: 'Velora Store',
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMood,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text('dsadsa'),
+              ),
+            ),
+          );
+        },);
   }
 }
