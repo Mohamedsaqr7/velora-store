28831076d7ceac3578a4c6863528547aa347ee0d
shekosakr <mmdsakr942@gmail.com>
2025-03-04 01:28:45 +0200

    add font done

--- lib/velora_app.dart
+++ lib/velora_app.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:velora/core/app/connectivity_plus.dart';
 import 'package:velora/core/app/env_variables.dart';
 import 'package:velora/core/common/no_network_screen.dart';
+import 'package:velora/core/style/font_family.dart';
+
+import 'core/style/font_weight.dart';
 
 class VeloraStore extends StatelessWidget {
   const VeloraStore({super.key});
@@ -18,7 +22,50 @@ class VeloraStore extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('dsadsa'),
+          title: const Text(
+            'data',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        body: const Center(
+          child: Column(
+            children: [
+              // Old
+              Text(
+                'Old Font',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                'محمد صقر',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              // New
+
+              Text(
+                'Old Font',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: FontFamily.poppinsEnglish,
+                  fontWeight: FontWeightHelper.bold,
+                ),
+              ),
+
+              Text(
+                'محمد صقر',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: FontFamily.cairoArabic,
+                  fontWeight: FontWeightHelper.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
