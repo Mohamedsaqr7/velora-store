24bbad2045078c93bc6c061fc32a870b61df8013
shekosakr <mmdsakr942@gmail.com>
2025-03-04 00:36:46 +0200

    Test commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'core/env_variables.dart';
+import 'core/app/env_variables.dart';
 import 'velora_app.dart';
 
 void main() async{
